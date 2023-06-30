d6f0d33c61b50f12dea73a644f1a45d2f0e154fe
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-06-30 10:45:38 -0700

    how to use the console

---
+++ cheatsheets/comments1.py
@@ -0,0 +1,12 @@
+# How to use the python console to find methods
+
+# Commands: 
+
+# >>> dir(str) will show all string methods you can use in python
+# >>> help(str.methodname) ex: >>> help(str.capitalize) will information about what this method does
+
+# >>> dir(list) will show all list methods you can use in python
+# >>> help(list.methodname) will give you information about the method
+
+# >>> import builtins to import python functions
+# >>> dir(builtins) will show you a list of functions that you can use in python
