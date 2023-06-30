3fb833c799e0124f95e2e7a16ec94aac1e63ec61
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-06-30 09:29:15 -0700

    taking a look at Boolean loops

--- app-1/main.py
+++ app-1/main.py
@@ -1,7 +1,10 @@
 user_prompt = "Enter a todo:"
 
+todos = []
 
+# Boolean loop. append is a method, methods can take arguments
 while True:
     todo = input(user_prompt)
-    print(todo)
-    print("Next...")
+    todos.append(todo)
+    print(todos)
+
