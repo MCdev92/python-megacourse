f5a200e3f496d4829459f8f7cc68b3ef5f0bd6f5
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-06-30 11:59:30 -0700

    for loop bonus

---
+++ app-1/bonus/bonus3.py
@@ -0,0 +1,7 @@
+drinks = ['soda', 'juice', 'water']
+
+# for loop runs similar to while loop. for loops run as long as there are items.
+for char in drinks:
+    print(char.capitalize())
+
+print("See ya")
