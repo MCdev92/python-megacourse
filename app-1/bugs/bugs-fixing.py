74242b05dd520b363ca11b90c771ee4126510772
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-06-30 10:31:12 -0700

    bugs hunting in python

---
+++ app-1/bugs/bugs-fixing.py
@@ -0,0 +1,46 @@
+# This line of code is missing the close brakets!
+answers = ['Yes', 'No', 'Yes', 'No', 'No'
+# ---------------------------------------------------------------------------------------------------
+# This line of code is missing the commas after 'YES' and 'NO'      
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes' 'No' my_answer]   
+# ---------------------------------------------------------------------------------------------------
+# This line of code id missing the "." on the input variable
+my_answer = input(What is your answer?)
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+# ---------------------------------------------------------------------------------------------------
+# This line of code contains the wrong brakets for the string passing the input variable
+my_answer = input["What is your answer?"]
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+# ---------------------------------------------------------------------------------------------------
+# This code is missing the : after True and indent after print
+while True
+print("Hello")
+
+#The programmer here is trying to convert the string "hello" to "HELLO" by using the upper() method:
+
+# The argument should be outside of the parentheses ex: print(greetin.upper())
+greeting = "hello"
+print(upper(greeting))
+
+# ---------------------------------------------------------------------------------------------------
+# However, the program returns an error. Can you help fix the code, so it prints out HELLO?
+
+# A programmer wrote the following program:
+
+countries = []
+ 
+while True:
+    country = input("Enter the country: ")
+    countries.append(country)
+print(countries)     #  --> This print function should be indented
+
+#The expected output is as follows:
+    Enter the country: Cambodia
+    ["Cambodia"]
+    Enter the country: Triomindia
+    ["Cambodia", "Triomindia"]
+    Enter the country
+
+#However, the code returns an error instead of the expected output. Fix the code, so it produces the expected output.
+# ---------------------------------------------------------------------------------------------------
