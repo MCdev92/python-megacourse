b4026f7be8b10f49c376b33f87e098607791c2be
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-06-30 09:10:19 -0700

    learning variables and functions PY

---
+++ app-1/bonus.py
@@ -0,0 +1,6 @@
+# This examples shows the use of input, len and print functions
+text = input("Enter a title: ")
+
+length = len(text)
+
+print("the length of the title:", length)
---
+++ app-1/main.py
@@ -0,0 +1,11 @@
+# user_prompt is a variable that has multiple todo functions
+user_prompt = "Enter a todo:"
+todo1 = input(user_prompt) 
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+print(todos)
+
+# This line of code will print the type 
+print(type(todo1))
