145d9fc245b0ee0717a60db22b52e7f94e19d627
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-06-30 09:10:35 -0700

    bugs fixing in python

---
+++ app-1/bugs-fixing.py
@@ -0,0 +1,14 @@
+# This line of code is missing the close brakets!
+answers = ['Yes', 'No', 'Yes', 'No', 'No'
+
+# This line of code is missing the commas after 'YES' and 'NO'      
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes' 'No' my_answer]   
+
+# This line of code id missing the "." on the input variable
+my_answer = input(What is your answer?)
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+# This line of code contains the wrong brakets for the string passing the input variable
+my_answer = input["What is your answer?"]
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
