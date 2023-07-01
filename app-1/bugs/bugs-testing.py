fca82b09fd4034f8e12c28b0b5acbccc833135f7
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-01 08:12:40 -0700

    testing

--- app-1/bugs/bugs-testing.py
+++ app-1/bugs/bugs-testing.py
@@ -1,2 +1,6 @@
-for item in ["sandals", "glasses", "trousers"]: 
-    print(item.capitalize())
+rate = 2
+
+dollars = float(input("How many dollars have you got? "))
+euros = dollars * rate
+    
+print(euros)
