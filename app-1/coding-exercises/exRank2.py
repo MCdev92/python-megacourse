1c67ca17102ed9dbd56cf3e7851e6f1baadd7d96
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-01 08:43:18 -0700

    ex

--- app-1/coding-exercises/exRank2.py
+++ app-1/coding-exercises/exRank2.py
@@ -1,3 +1,11 @@
+# create a program that:
+
+# 1. Contains the above list.
+
+# 2 Prompts the user to input the person's name.
+
+# 3. Returns the rank that person has.
+
 ranking = ['John', 'Sen', 'Lisa']
  
 # Prompt the user to input a person's name and assign it to the variable 'name'.
