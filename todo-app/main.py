a985b8eb974977d78563ec7675c3ccfc52b69fc7
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-01 09:34:18 -0700

    added {index +1} and create case 'complete'

--- todo-app/main.py
+++ todo-app/main.py
@@ -2,7 +2,7 @@ todos = []
 
 # For loop using match to allow the user to choose an action
 while True: 
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -11,13 +11,16 @@ while True:
             todos.append(todo)
         case 'show':
             for index, item in enumerate(todos): # adding the func enumerate will show number of items
-                row = f"{index}-{item}" # Define a new variable named row with f string {index}-{items} to avoid extra space
+                row = f"{index + 1}-{item}" #  {index + 1} will make our list start from 1 not zero
                 print(row) 
         case 'edit':
             number = int(input("Number of todo to edit: ")) # convert from str to int
             number = number - 1 # This variable will allow the user to choose the right number  i.e: 1-1 = 0, 1-2 = 1
             new_todo = input("Enter new todo: ") # this variable will store new user input in new_todo
             todos[number] = new_todo # Access items from a list and replace
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1) # this will make our list start from 1 not zero
         case 'exit':
             break
     
