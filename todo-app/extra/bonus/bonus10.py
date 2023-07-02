306e5902e092926a9e3f150ee3b39806693e4c5f
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-02 11:51:53 -0700

    logical error handling (try and except)

---
+++ todo-app/extra/bonus/bonus10.py
@@ -0,0 +1,12 @@
+# This is a logical error only can be handle by a human
+try:
+    width = float(input("Enter rectangle width: "))
+    length = float(input("Enter rectangle length: "))
+    
+    if width == length:
+        exit("That looks like a square.")
+    
+    area = width * length
+    print(area)
+except ValueError: 
+    print("Please enter a number.")
