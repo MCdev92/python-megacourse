8ff340efc838017d366fda23cafd667b16d905d6
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-02 09:06:22 -0700

    rm extra .txt from file ovwewrite

--- journal-app/journalApp.py
+++ journal-app/journalApp.py
@@ -2,6 +2,6 @@ date = input("Enter today's date: ")
 mood = input("Please rate your mood from 1 to 10: ")
 thoughts = input("Let your thoughts flow:\n")
 
-with open(f"journal-app/journal-files/journal.txt{date}.txt", 'w') as file:
+with open(f"journal-app/journal-files/journal{date}.txt", 'w') as file:
     file.write(mood + 2 * "\n")
     file.write(thoughts)
