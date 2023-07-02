4eab99429a168a8647c40e248ca0b42690f0da6b
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-01 21:44:28 -0700

    added a for loop to clean up the list

--- todo-app/main.py
+++ todo-app/main.py
@@ -20,7 +20,16 @@ while True:
                 todos = file.readlines() 
                 file.close() 
             
-                for index, item in enumerate(todos): 
+            # This new for loop will clean up the list "remover the spaces" from the list
+                new_todos = []
+                
+                for item in todos:
+                    new_item = item.strip('\n')
+                    new_todos.append(new_item)
+                    
+            # This new for loop will clean up the list "remover the spaces" from the list
+                    
+                for index, item in enumerate(new_todos): 
                     row = f"{index + 1}-{item}" 
                     print(row)
  
