cf6f1b90c57a155e6d121b750965ed8ee6f3141b
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-01 21:25:48 -0700

    rm comments to clean code struct

--- todo-app/main.py
+++ todo-app/main.py
@@ -4,34 +4,34 @@ while True:
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ") + "\n" # + "\n" will create a space between the items
+            todo = input("Enter a todo: ") + "\n" 
             
-            file = open("todo-app/files/todos.txt", 'r') # this command will open the file in read mode
-            todos = file.readlines() # this commannd reads and creates a list of what is in the file
-            file.close() # we always need to use the close command
+            file = open("todo-app/files/todos.txt", 'r') 
+            todos = file.readlines() 
+            file.close() 
             
-            todos.append(todo) # this command will append the items the user inputs
+            todos.append(todo) 
             
-            file = open("todo-app/files/todos.txt", 'w') # this overwrites the existing file 
+            file = open("todo-app/files/todos.txt", 'w') 
             file.writelines(todos)
             file.close()
         case 'show':
                 file = open("todo-app/files/todos.txt", 'r')
-                todos = file.readlines() # this commannd reads and creates a list of what is in the file
-                file.close() # we always need to use the close command
+                todos = file.readlines() 
+                file.close() 
             
-                for index, item in enumerate(todos): # adding the func enumerate will show number of items
-                    row = f"{index + 1}-{item}" #  {index + 1} will make our list start from 1 not zero
+                for index, item in enumerate(todos): 
+                    row = f"{index + 1}-{item}" 
                     print(row)
  
         case 'edit':
-            number = int(input("Number of todo to edit: ")) # convert from str to int
-            number = number - 1 # This variable will allow the user to choose the right number  i.e: 1-1 = 0, 1-2 = 1
-            new_todo = input("Enter new todo: ") # this variable will store new user input in new_todo
-            todos[number] = new_todo # Access items from a list and replace
+            number = int(input("Number of todo to edit: ")) 
+            number = number - 1 
+            new_todo = input("Enter new todo: ") 
+            todos[number] = new_todo 
         case 'complete':
             number = int(input("Number of the todo to complete: "))
-            todos.pop(number - 1) # this will make our list start from 1 not zero
+            todos.pop(number - 1) 
         case 'exit':
             break
     
