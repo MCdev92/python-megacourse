e8add84e931f8e93c9c24fa0193f5e7c6e501bdb
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-02 08:37:17 -0700

    strip breakline from todo_to_index var

--- todo-app/main.py
+++ todo-app/main.py
@@ -48,7 +48,7 @@ while True:
             with open("todo-app/files/todos.txt", 'r') as file: 
                 todos = file.readlines()
             index = number -1 
-            todo_to_remove = todos[index]    
+            todo_to_remove = todos[index].strip('\n') 
             todos.pop(index) 
             
             with open("todo-app/files/todos.txt", 'w') as file:
