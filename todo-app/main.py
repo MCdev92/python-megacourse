7845272629131154b0168dd94cfa6b5a95c690e4
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-02 11:30:37 -0700

    added .starwith() method to improve if else s.

--- todo-app/main.py
+++ todo-app/main.py
@@ -2,18 +2,18 @@ while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    if 'add' in user_action:
+    if user_action.startswith('add'):
         todo = user_action[4:] 
         
         with open("todo-app/todos.txt", 'r') as file: 
             todos = file.readlines()
             
-        todos.append(todo) 
+        todos.append(todo + '\n')
         
         with open("todo-app/todos.txt", 'w') as file:
             file.writelines(todos)
 
-    elif 'show' in user_action:
+    elif user_action.startswith('show'):
             
         with open("todo-app/todos.txt", 'r') as file:
             todos = file.readlines()
@@ -23,7 +23,7 @@ while True:
                 row = f"{index + 1}-{item}" 
                 print(row)
 
-    elif 'edit' in user_action:
+    elif user_action.startswith('edit'):
         number = int(user_action[5:]) 
         print(number)
         
@@ -38,7 +38,7 @@ while True:
         with open("todo-app/todos.txt", 'w') as file:
             file.writelines(todos)
         
-    elif 'complete' in user_action:
+    elif user_action.startswith('complete'):
         number = int(user_action[9:])
         
         with open("todo-app/todos.txt", 'r') as file: 
@@ -53,7 +53,7 @@ while True:
         message = f"Todo {todo_to_remove} was removed from the list."
         print(message)   
             
-    elif 'exit' in user_action:
+    elif user_action.startswith('exit'):
         break
     else:
         print("Error Command is not Supported")
