f59a035a3707525e2ad3fed22b62eea9283a84fa
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-03 09:23:13 -0700

    Intro to doc-strings

--- todo-app/main.py
+++ todo-app/main.py
@@ -1,10 +1,12 @@
-# Custom functions can be created with def, need to be place outside of the code
+# doc-strings are very important when using fuctions for multiple programs and colab
 def get_todos(filepath="todo-app/todos.txt"):
+    """ Read a txt file and return the list of to-do items. """
     with open(filepath, 'r') as file_local: 
         todos_local = file_local.readlines()
     return todos_local
 
 def write_todos(todos_arg, filepath="todo-app/todos.txt"):
+    """ Write the to-do items list in the text file. """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
             
