750153b10430f38b77b18a17d6d9d3bd9c281882
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-03 09:40:53 -0700

    optimazed the code by decopling functions

--- heightcheck-app/heightcheck.py
+++ heightcheck-app/heightcheck.py
@@ -1,17 +1,24 @@
 # This is a simple app that checks if a kid has the required height to use the ride
 # This app will use decopling inside a custom function definition to convert ft and in to height
-
+# optimization added: created parse to store the var parts, feet and inch.
+# then only use the convert func to convert feet and inch to meters
 feet_inches = input("Enter feet and inches: ")
 
-def convert(feet_inches):
+def parse(feet_inches):
     parts = feet_inches.split(" ")
     feet = float(parts[0])
     inches = float(parts[1])
-    
+    return {"feet": feet, "inches": inches}
+
+def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
     return meters
+ 
+parsed = parse(feet_inches)
 
-result = convert(feet_inches)
+result = convert(parsed['feet'], parsed['inches'])
+
+print(f"{parsed['feet']} feet and {parsed['inches']} is equal to {result}")
 
 if result < 1:
     print("Sorry you are too small to enter te ride.")
