d7a787ecfc1d188b28fbff150e5235ee8ec4f457
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-03 10:49:02 -0700

    Intro to CONSTANTS

--- todo-app/functions.py
+++ todo-app/functions.py
@@ -1,11 +1,14 @@
+# CONSTANT added to custom fucntion
+FILEPATH = "todo-app/todos.txt"
+
 # doc-strings are very important when using fuctions for multiple programs and colab
-def get_todos(filepath="todo-app/todos.txt"):
+def get_todos(filepath=FILEPATH):
     """ Read a txt file and return the list of to-do items. """
     with open(filepath, 'r') as file_local: 
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath="todo-app/todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file. """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
