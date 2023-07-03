c0ffeecac4d5574a0fb914c0d87f74451cf52ee6
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-03 09:12:18 -0700

    intro to default arguments

--- todo-app/main.py
+++ todo-app/main.py
@@ -1,10 +1,10 @@
 # Custom functions can be created with def, need to be place outside of the code
-def get_todos(filepath):
+def get_todos(filepath="todo-app/todos.txt"):
     with open(filepath, 'r') as file_local: 
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath="todo-app/todos.txt"):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
             
@@ -15,15 +15,15 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:] 
         
-        todos = get_todos("todo-app/todos.txt") # function called here under todos variable
+        todos = get_todos()
             
         todos.append(todo + '\n')
         
-        write_todos("todo-app/todos.txt", todos)
+        write_todos(todos)
 
     elif user_action.startswith('show'):
             
-            todos = get_todos("todo-app/todos.txt")
+            todos = get_todos()
             
             for index, item in enumerate(todos): 
                 item = item.strip('\n') 
@@ -38,12 +38,12 @@ while True:
             
             number = number - 1 
             
-            todos = get_todos("todo-app/todos.txt")
+            todos = get_todos()
             
             new_todo = input("Enter new todo: ") 
             todos[number] = new_todo + '\n'
             
-            write_todos("todo-app/todos.txt", todos)
+            write_todos(todos)
         except ValueError:
             print("Your command is not valid")
             continue
@@ -53,12 +53,12 @@ while True:
         try:
             number = int(user_action[9:])
             
-            todos = get_todos("todo-app/todos.txt")
+            todos = get_todos()
             index = number -1 
             todo_to_remove = todos[index].strip('\n') 
             todos.pop(index) 
             
-            write_todos("todo-app/todos.txt", todos)
+            write_todos(todos)
                 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)  
