763b79631501553d52afe507824ba00f5124db5b
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-06 10:39:31 -0700

    testing testing

--- todo-app/extra/bugs/bugs-testing.py
+++
@@ -1,9 +0,0 @@
-try:
-    waiting_list = ["john", "marry"]
-    name = input("Enter name: ")
-    
-    number = waiting_list.index(name)
-    print(f"{name}'s turn is {number}")
-except ValueError:
-    print(f"{name} is not on the list")
-    
