42c11697e0b43948436eccf4edf60d35a4b766eb
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-07 14:12:38 -0700

    added lines to pdf file!

--- Intermediate/pdf-template/main.py
+++ Intermediate/pdf-template/main.py
@@ -13,7 +13,12 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
              ln=1)
-    pdf.line(10, 21, 200, 21)
+    # commented out clear pages with no lines
+    # pdf.line(10, 21, 200, 21) 
+    
+    for y in range(20, 298, 10): # add lines to your pages
+        pdf.line(10, y, 200, y)
+    
     
     # Set the footer master
     pdf.ln(265)
@@ -25,9 +30,12 @@ for index, row in df.iterrows():
         pdf.add_page()
             
         # Set the footer
-        pdf.ln(265)
+        pdf.ln(275)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180,180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        
+        for y in range(20, 298, 10): # add lines to your pages
+            pdf.line(10, y, 200, y)
         
 pdf.output("output.pdf")
