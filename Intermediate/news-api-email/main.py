e4091746e6f84e2ed9b3e90ec6d1d36eebe18c1f
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-08 15:28:37 -0700

    extra lines rm

--- Intermediate/news-api-email/main.py
+++ Intermediate/news-api-email/main.py
@@ -15,8 +15,8 @@ content = request.json()
 # Access the article titles and description
 body = ""
 for article in content["articles"][:20]:
-    body = "Subject: Today's news" + "\n" + body + article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
+    if article["title"] is not None:
+        body = "Subject: Today's news" + "\n" + body + article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
-    
