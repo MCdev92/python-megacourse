bb5e91274f2a56e7e82ab377d618e8fab5f32e30
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-08 08:12:16 -0700

    Added an invoice date

--- Intermediate/invoice-generation/app.py
+++ Intermediate/invoice-generation/app.py
@@ -6,11 +6,18 @@ from pathlib import Path
 filepaths = glob.glob("intermediate/invoice-generation/invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    df = pd.read_excel(filepath, sheet_name="Sheet 1") #read
+    
+    pdf = FPDF(orientation="P", unit="mm", format="A4") # create
     pdf.add_page()
-    filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
-    pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    
+    filename = Path(filepath).stem # Get filename
+    invoice_nr, date = filename.split("-") # Get invoice
+    
+    pdf.set_font(family="Times", size=16, style="B") 
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1) 
+    
+    pdf.set_font(family="Times", size=16, style="B") 
+    pdf.cell(w=50, h=8, txt=f"Date:{date}") # create cell
+
     pdf.output(f"intermediate/invoice-generation/PDFs/{filename}.pdf")
