75e4490e3af7c29e980ce97dee40333912c90413
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-13 11:54:49 -0700

    introduction to threads

--- Intermediate/email-webcam-detection/emailing.py
+++ Intermediate/email-webcam-detection/emailing.py
@@ -8,10 +8,11 @@ SENDER = "corporancarriers@gmail.com"
 RECEIVER = "corporancarriers@gmail.com"
 
 def send_email(image_path):
+    print("send_email function started")
     email_message = EmailMessage()
     email_message["Subject"] = "New object entered the camera range!"
     email_message.set_content("Hey a new object enter the camera range!")
-
+    
     with open(image_path, "rb") as file:
         content = file.read()
     email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
@@ -22,6 +23,7 @@ def send_email(image_path):
     gmail.login(SENDER, PASSWORD)
     gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
     gmail.quit()
+    print("send_email function ended")
     
 if __name__ == "__main__":
     send_email(image_path="Intermediate/email-webcam-detection/images/10.png")
--- Intermediate/email-webcam-detection/main.py
+++ Intermediate/email-webcam-detection/main.py
@@ -2,6 +2,7 @@ import cv2
 import time
 import os
 import glob
+from threading import Thread
 from emailing import send_email
 
 video = cv2.VideoCapture(1)
@@ -14,9 +15,11 @@ count = 1
 
 
 def clean_folder():
+    print("clean_folder function started")
     images = glob.glob("Intermediate/email-webcam-detection/images/*.png")
     for image in images:
         os.remove(images)
+    print("clean_folder function ended")
         
         
 while True:
@@ -55,9 +58,14 @@ while True:
     status_list = status_list[-2:]
     
     if status_list[0] == 1 and status_list[1] == 0:
-         send_email(image_with_object)
-         clean_folder()
-         
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+        
+        email_thread.start()
+        clean_thread.star()
+
     print(status_list)
             
     cv2.imshow("video", frame)
