161125edcc141c0b01d583cd30e9d686b741cf1d
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-13 11:10:24 -0700

    created complete send_email script!

--- Intermediate/email-webcam-detection/emailing.py
+++ Intermediate/email-webcam-detection/emailing.py
@@ -1,2 +1,27 @@
-def send_email():
-    print("Email was sent")
+import os
+import imghdr
+import smtplib, ssl
+from email.message import EmailMessage
+
+PASSWORD = os.getenv("PASSWORD")
+SENDER = "corporancarriers@gmail.com"
+RECEIVER = "corporancarriers@gmail.com"
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New object entered the camera range!"
+    email_message.set_content("Hey a new object enter the camera range!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+    
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+    
+if __name__ == "__main__":
+    send_email(image_path="Intermediate/email-webcam-detection/images/10.png")
