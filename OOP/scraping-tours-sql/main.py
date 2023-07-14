1459f62c85561e324ec07e7b0f4a586bb634a69a
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-14 12:46:39 -0700

    program works

--- OOP/scraping-tours-sql/main.py
+++ OOP/scraping-tours-sql/main.py
@@ -6,7 +6,6 @@ import selectorlib
 import smtplib, ssl
 
 
-
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
