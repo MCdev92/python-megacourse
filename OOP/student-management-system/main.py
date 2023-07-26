f81e6ffc52544ed0eab977977775468b0cf855f2
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-07-26 11:21:41 -0700

    added movable toolbar and tbar elements

--- OOP/student-management-system/main.py
+++ OOP/student-management-system/main.py
@@ -1,20 +1,21 @@
 import sys        
 import sqlite3
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QAction                                                       
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QGridLayout, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, QComboBox
+from PyQt6.QtGui import QAction, QIcon                                                       
+from PyQt6.QtWidgets import QApplication, QToolBar, QStatusBar, QWidget, QLabel, QLineEdit, QGridLayout, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, QComboBox
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
+        self.setMinimumSize(800, 600)
         
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
         edit_menu_item = self.menuBar().addMenu("&Edit")
         
-        add_student_action = QAction("Add Student", self)
+        add_student_action = QAction(QIcon("OOP/student-management-system/icons/add.png"),"Add Student", self)
         add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
         
@@ -22,7 +23,7 @@ class MainWindow(QMainWindow):
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
         
-        search_action = QAction("Search", self)
+        search_action = QAction(QIcon("OOP/student-management-system/icons/search.png"),"Search", self)
         edit_menu_item.addAction(search_action)
         search_action.triggered.connect(self.search)
         
@@ -33,7 +34,16 @@ class MainWindow(QMainWindow):
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
         
+        # Create toolbar and add toolbar elements
+        toolbar = QToolBar()
+        toolbar.setMovable(True)
+        self.addToolBar(toolbar)
         
+        toolbar.addAction(add_student_action)
+        toolbar.addAction(search_action)
+        
+        # Create status bar and add status bar elements
+        statusbar = QStatusBar()
     def load_data(self):
         connection = sqlite3.connect("OOP/student-management-system/database.db")
         result = connection.execute("SELECT * FROM students")
