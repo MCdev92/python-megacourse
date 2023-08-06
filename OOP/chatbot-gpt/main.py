52415dea77b50313b6f176a3e9992964a51bc8f5
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-08-06 09:08:11 -0700

    final tweaks!

--- OOP/chatbot-gpt/main.py
+++ OOP/chatbot-gpt/main.py
@@ -9,22 +9,22 @@ class ChatbotWindow(QMainWindow):
         super().__init__()
         
         self.chatbot = Chatbot()
-        
-        self.setMinimumSize(700, 500)
+        self.setWindowTitle("ChatBot by McDev92")
+        self.setMinimumSize(640, 450)
         
         # Add chat are widget
         self.chat_area = QTextEdit(self)
-        self.chat_area.setGeometry(10, 10, 480, 320)
+        self.chat_area.setGeometry(10, 20, 480, 320)
         self.chat_area.setReadOnly(True)
         
         # Add the input field widget
         self.input_field = QLineEdit(self)
-        self.input_field.setGeometry(10, 340, 480, 40)
+        self.input_field.setGeometry(10, 360, 480, 40)
         self.input_field.returnPressed.connect(self.send_message)
         
         # Add the button
         self.button = QPushButton("Send", self)
-        self.button.setGeometry(500, 340, 100, 40)
+        self.button.setGeometry(500, 360, 100, 40)
         self.button.clicked.connect(self.send_message)
         
         self.show()
